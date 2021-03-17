197b7e1ec639bf44ceb7edf2e367ca52b9b69e4a
lealvjo <leonardo.jose.barros@hotmail.com>
2021-03-17 13:59:01 -0300

    update env and mvn test

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -2,11 +2,14 @@ package config;
 
 import java.io.IOException;
 
-public class Config extends Environment{
+public class Config extends Environment {
 
     private String configuration = System.getProperty("ENVIRONMENT");
 
     public String getUrlEnvironment() throws IOException {
+        if (configuration == null) {
+            configuration = "LOCAL";
+        }
         String environment = getEnvironment(configuration);
         return environment;
     }
